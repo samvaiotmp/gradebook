b80a731a8d6854bd2032308623a15a5afc3f4b2e
samvaio <samvaiotmp@gmail.com>
2017-07-05 00:07:40 -0500

    version 1 mac

---
+++ Grades/GradeBook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Grades
+{
+    class GradeBook
+    {
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        List<float> grades = new List<float>();
+    }
+}
